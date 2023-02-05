291767eb302ba7dfcaeea7df0d59e0ce9cc80c88
nastyashvec42 <nastyashvec42@gmail.com>
2023-02-05 17:57:22 +0300

    Lesson 1

---
+++ src/02/02.ts
@@ -0,0 +1,52 @@
+type TechnologiesPropsType = {
+    id:number
+    title: string
+}
+type CityPropsType = {
+    title: string
+    countryTitle: string
+}
+type AddressPropsType = {
+    streetTitle: string
+    city: CityPropsType
+}
+type StudentPropsType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressPropsType
+    technologies: Array<TechnologiesPropsType>
+}
+
+const student: StudentPropsType = {
+    id:1,
+    name: "Dimych",
+    age: 32,
+    isActive: false,
+    address: {
+        streetTitle: "Surganova 2",
+        city: {
+            title: "Minsk",
+            countryTitle: "Belarus"
+        }
+    },
+    technologies: [
+        {
+            id:1,
+            title: "HTML"
+        },
+        {
+            id:2,
+            title: "CSS"
+        },
+        {
+            id:3,
+            title: "React"
+        }
+    ]
+}
+console.log(student.age)
+console.log(student.name)
+console.log(student.address.city.title)
+console.log(student.technologies[2].title)
